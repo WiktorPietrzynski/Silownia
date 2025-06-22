6e6126587db080be5f82f041965c7e6b54cf7a23
WiktorPietrzynski <wiktor.pietrzynski@o2.pl>
2025-06-22 12:04:53 +0200

    add client method, add instructor method

--- src/silownia.py
+++ src/silownia.py
@@ -12,3 +12,16 @@ class Silownia:
         self.klienci = []
         self.zajecia = []
 
+    def dodaj_klienta(self, klient: Klient):
+        if klient in self.klienci:
+            print(f"Klient {klient.imie} {klient.nazwisko} już istnieje.")
+        else:
+            self.klienci.append(klient)
+            print(f"Dodano klienta: {klient.imie} {klient.nazwisko}")
+
+    def dodaj_instruktora(self, instruktor: Instruktor):
+        if instruktor in self.instuktorzy:
+            print(f"Instruktor {instruktor.imie} {instruktor.nazwisko} już istnieje.")
+        else:
+            self.instuktorzy.append(instruktor)
+            print(f"Dodano instruktora: {instruktor.imie} {instruktor.nazwisko}")
