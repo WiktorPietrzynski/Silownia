765706dd2338a446c356181b2e0a40d4afae5913
WiktorPietrzynski <wiktor.pietrzynski@o2.pl>
2025-06-22 12:22:01 +0200

    add show instructors method, show classes method

--- src/silownia.py
+++ src/silownia.py
@@ -39,3 +39,46 @@ class Silownia:
         if zajecia.data in godziny_rozpoczecia_zajec:
             raise ValueError("Zajecia nie mogą odbywać się w tym samym czasie")
 
+    def wyswietl_czlonkow(self):
+        ilosc_czlonkow = len(self.klienci)
+        aktywni_czlonkowie = [f"{klient.imie} {klient.nazwisko}" for klient in self.klienci if klient.status == "aktywny"]
+        ilosc_aktywnych_czlonkow = len(aktywni_czlonkowie)
+        nieaktywni_czlonkowie = [f"{klient.imie} {klient.nazwisko}" for klient in self.klienci if klient.status == "nieaktywny"]
+        ilosc_nieaktywnych_czlonkow = len(nieaktywni_czlonkowie)
+        print(f"Siłownia {self.nazwa} posiada {ilosc_czlonkow} członków.")
+        print(f"{ilosc_aktywnych_czlonkow} z {ilosc_czlonkow} posiada aktywny karnet.")
+        print(f"{ilosc_nieaktywnych_czlonkow} z {ilosc_czlonkow} nie opłaciło swojego karnetu za ten miesiąc.")
+
+    def wyswietl_instruktorow(self):
+        ilosc_instruktorow = len(self.instruktorzy)
+        lista_specjalizacji = []
+        for insturktor in self.instruktorzy:
+            lista_specjalizacji.extend(insturktor.specjalizacje)
+        unikalne_specjalizacje = list(set(lista_specjalizacji))
+        ilosc_unikalnych_specalizacji = len(unikalne_specjalizacje)
+        print(f"Siłownia {self.nazwa} zatrudnia {ilosc_instruktorow}")
+        print(f"Specjalizacje instruktorów dotyczą {ilosc_unikalnych_specalizacji} obszarów: {', '.join(unikalne_specjalizacje)}")
+        print("Lista instruktorów:")
+        for instruktor in self.instruktorzy:
+            print(f"- {instruktor.imie} {instruktor.nazwisko}, specjalizacje: {', '.join(instruktor.specjalizacje)}")
+
+    def wyswietl_zajecia(self):
+        aktualny_czas = datetime.now()
+        zrealizowane_zajecia = [zajecia for zajecia in self.zajecia if zajecia.data < aktualny_czas]
+        ilosc_zrealizowanych_zajec = len(zrealizowane_zajecia)
+        zaplanowane_zajecia = [zajecia for zajecia in self.zajecia if zajecia.data >= aktualny_czas]
+        ilosc_zaplanowanych_zajec = len(zaplanowane_zajecia)
+        print(f"Zajęcia w siłowni {self.nazwa}:")
+        if zrealizowane_zajecia:
+            print(f"Zrealizowano {ilosc_zrealizowanych_zajec} zajęć:")
+            for zajecia in zrealizowane_zajecia:
+                print(f"- {zajecia.nazwa} - {zajecia.data}")
+        else:
+            print("Brak zrealizowanych zajęć.")
+        if zaplanowane_zajecia:
+            print(f"Zaplanowano {ilosc_zaplanowanych_zajec} zajęć:")
+            for zajecia in zaplanowane_zajecia:
+                print(f"- {zajecia.nazwa} - {zajecia.data}")
+        else:
+            print("Brak zaplanowanych zajęć.")
+
